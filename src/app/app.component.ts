14252ab92f54367a782a44f1d0aacbaac757e508
KryptonBD <foysol.sahi@gmail.com>
2024-10-06 12:33:20 +0100

    Base setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CounterControlsComponent } from './counter-controls/counter-controls.component';
+import { CounterOutputComponent } from './counter-output/counter-output.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    CounterControlsComponent,
+    CounterOutputComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ngrx-counter';
-}
+export class AppComponent {}
