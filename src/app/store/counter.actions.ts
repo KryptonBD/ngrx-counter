ee34b51a0124e39e3fe52625a0c6423653dd2beb
KryptonBD <foysol.sahi@gmail.com>
2024-10-07 10:52:17 +0100

    Reducer and actions

---
+++ src/app/store/counter.actions.ts
@@ -0,0 +1,11 @@
+import { createAction, props } from '@ngrx/store';
+
+export const increment = createAction(
+  '[Counter] increment',
+  props<{ value: number }>()
+);
+
+export const decrement = createAction(
+  '[Counter] decrement',
+  props<{ value: number }>()
+);
